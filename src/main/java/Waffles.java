fa9bc1e6d77f8eea64c12be76d91845f2b1b89a1
Ang Ee Yang <E1525339@u.nus.edu>
2026-08-21 15:28:43 +0800

    Replaced name, added greeting and exit

---
+++ src/main/java/Waffles.java
@@ -0,0 +1,21 @@
+/**
+ * The entry point for the Waffles chatbot.
+ */
+public class Waffles {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = " __        __    _      _____ _____ _      _____  ____\n"
+                + " \\ \\      / /   / \\    |  ___|  ___| |    | ____|/ ___|\n"
+                + "  \\ \\ /\\ / /   / _ \\   | |_  | |_  | |    |  _|  \\___ \\ \n"
+                + "   \\ V  V /   / ___ \\  |  _| |  _| | |___ | |___  ___) |\n"
+                + "    \\_/\\_/   /_/   \\_\\ |_|   |_|   |_____||_____||____/";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Waffles.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
